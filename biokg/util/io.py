71086090b101e02c690db2404117a5aee27ca96a
matteo01bonato01 <102580596+matteo01bonato01@users.noreply.github.com>
2024-03-06 15:42:24 +0100

    Update io.py
    aggiunta riga 247 download_file invece di download_file_with_aut

--- biokg/util/io.py
+++ biokg/util/io.py
@@ -244,7 +244,8 @@ def download_file_md5_check(download_url, filepath, username=None, password=None
         if username is None or password is None:
             download_file(download_url, filepath)
         else:
-            download_file_with_auth(download_url, filepath, username, password)
+            "download_file_with_auth(download_url, filepath, username, password)"
+            download_file(download_url, filepath, username, password)
         download_time = timer() - start
         print(done_sym + " %1.2f Seconds." % download_time, end="", flush=True)
         file_computed_md5 = get_file_md5(filepath)
